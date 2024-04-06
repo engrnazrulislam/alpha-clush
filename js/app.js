deaee456647acfc75c16b3dd54c7984118d10d38
engrnazrulislam <nazrul.cmt12@gmail.com>
2024-04-07 00:34:49 +0600

    Alpha-clash Game Completed2

--- js/app.js
+++ js/app.js
@@ -1,33 +1,22 @@
 
 
-//Get a random alphabets
-function getRandomAlphabets(){
-    const alphabetString="abcdefghijklmnopqrstuvwxyz";
-    const alphabets=alphabetString.split('');
-
-//Get a random generates index
-    const randomNumber= Math.random()*25;
-    const index=Math.round(randomNumber);
-    const alphabet=alphabets[index];
-    return alphabet;
-}
-
+// Game Started by displaying playground and hidden home screen
 function displayPlayground(hideElementID, displayElementID) {
     hideScreen(hideElementID);
     displayScreen(displayElementID);
     continueGame();
 }
-
+// Key background Set Function
 function setBackgroundColorById(id){
     const element=document.getElementById(id);
     element.classList.add('bg-orange-400');
 }
-
+// Key Background Remove Function
 function removeBackgroundColorById(id){
     const element=document.getElementById(id);
     element.classList.remove('bg-orange-400');
 }
-
+// Keyboard Button Pressed Event Handle
 function handleKeyboardButtonPress(event){
     const playerPressed=event.key;
     const playerPressedAlphabet=playerPressed.toLocaleLowerCase();
@@ -50,6 +39,7 @@ function handleKeyboardButtonPress(event){
         }
     }
 }
+// Score Update Function
     function scoreUpdate(){
         const scoreElement=document.getElementById('scoreNumber');
         const scoreText=scoreElement.innerText;
@@ -58,7 +48,7 @@ function handleKeyboardButtonPress(event){
         scoreElement.innerText=newScore;
         return newScore;
     }
-
+    //Life Decreased Function
     function lifeDecreased(){
         const lifeElement=document.getElementById('lifeNumber');
         const lifeText=lifeElement.innerText;
@@ -69,6 +59,18 @@ function handleKeyboardButtonPress(event){
     }
 // capture keyboard key pressed
 document.addEventListener('keyup', handleKeyboardButtonPress);
+
+//Get a random alphabets
+function getRandomAlphabets(){
+    const alphabetString="abcdefghijklmnopqrstuvwxyz";
+    const alphabets=alphabetString.split('');
+
+//Get a random generates index
+    const randomNumber= Math.random()*25;
+    const index=Math.round(randomNumber);
+    const alphabet=alphabets[index];
+    return alphabet;
+}
 
 function continueGame(){
     // step-1 generate a random alphabet
@@ -88,10 +90,12 @@ function playAgain(hideId,displayId){
     document.getElementById('scoreNumber').innerText=0;
 }
 
+// Section Displaying
 function displayScreen(id) {
     const playNowElement = document.getElementById(id);
     playNowElement.classList.remove('hidden');
 }
+//Section Hiding
 function hideScreen(id) {
     const playGroundElement = document.getElementById(id);
     playGroundElement.classList.add('hidden');
